ee24a97ca20fdd30e933a66990de5762dad1bdab
sprtrpr <torres.shawn@gmail.com>
2021-07-26 17:59:36 -0400

    Refined game. Added win and lose screen design. Play agagin buttons.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 var num = Math.floor(Math.random() * 100) + 1;
-        var tries = 0;
-        function Guess() {
+var tries = 0;
+
+function Guess() {
             var g = document.form1.guess1.value;
             tries++;
             status = "Tries: " + tries;
@@ -9,11 +10,42 @@ var num = Math.floor(Math.random() * 100) + 1;
             if (g > num)
                 document.form1.hint.value = "No, guess lower.";
             if (g == num) {
-                window.alert("Correct!the number was: " + num + "\n You guessed it in " + tries + " tries.");
-                location.reload();
+                //window.alert("Correct!the number was: " + num + "\n You guessed it in " + tries + " tries.");
+                //location.reload();
+                function winfadeIn() {
+                    document.getElementsByClassName('uWon')[0].style.display = 'block';
+                    var fade = document.getElementsByClassName('uWon')[0];
+                    var opacity = 0;
+                    var intervalID = setInterval(function() {
+                        if (opacity < 1) {
+                            opacity = opacity + 0.1
+                            fade.style.opacity = opacity;
+                        } else {
+                            clearInterval(intervalID);
+                        }
+                    }, 50);
+                }
+                document.getElementById('answer').innerHTML = num;
+                document.getElementById('numTries').innerHTML = tries;
+                winfadeIn();
             }
             if (tries == 10) {
-                window.alert("Sorry! Times Up, the number was: " + num );
-                location.reload(); 
+                //window.alert("Sorry! Times Up, the number was: " + num );
+                //location.reload(); 
+                function lostfadeIn() {
+                    document.getElementsByClassName('sry')[0].style.display = 'block';
+                    var fade = document.getElementsByClassName('sry')[0];
+                    var opacity = 0;
+                    var intervalID = setInterval(function() {
+                        if (opacity < 1) {
+                            opacity = opacity + 0.1
+                            fade.style.opacity = opacity;
+                        } else {
+                            clearInterval(intervalID);
+                        }
+                    }, 50);
+                }
+                document.getElementById('answer2').innerHTML = num;
+                lostfadeIn();
             }
         }
